ead7e1148d420f801c759a73552c50f3c22e493e
Maksym-Tomusiak <maxtomusiak315@gmail.com>
2026-03-21 18:16:09 +0200

    fix: update config file

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export",
   reactCompiler: true,
+  basePath: "/Password-Generator",
 };
 
 export default nextConfig;
