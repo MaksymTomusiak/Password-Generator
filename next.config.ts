ec08d42535b016df76d8871e7ddc7ce44c3bbaf3
Maksym-Tomusiak <maxtomusiak315@gmail.com>
2026-03-21 18:58:47 +0200

    feat: Make next.config.ts dynamic for Vercel and GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,19 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  output: "export",
+// Check if the build is running inside GitHub Actions
+const isGithubActions = process.env.GITHUB_ACTIONS === "true";
+
+let nextConfig: NextConfig = {
   reactCompiler: true,
-  basePath: "/Password-Generator",
 };
+
+// Only apply these settings if deploying to GitHub Pages
+if (isGithubActions) {
+  nextConfig = {
+    ...nextConfig,
+    output: "export",
+    basePath: "/password-generator",
+  };
+}
 
 export default nextConfig;
